3f8a44f7f6797c7de250d66b3949483ccb12e0d3
Matthew Tjandera <matthewtjandera@gmail.com>
2025-05-04 10:48:48 +0700

    Create password maker.py

---
+++ password maker.py
@@ -0,0 +1,11 @@
+import random
+
+alphabet = 'abcdefghijklmnopqrstupwxyz1234567890~!@#$%^&*_+:"<>?|'
+answer = str(input('would you like a new 16 letter passport generated for you? Yes/no ')).lower()
+if answer == 'yes': 
+    password = ''.join(random.choices(alphabet, k=16))  # k = length of password
+    print(password)
+elif answer == 'no':
+    print('thank you and goodbye.')
+else:
+    print('error')
